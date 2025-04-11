de2e499c3fa3512ce126dbd13d22f372c20d2394
snehal-volt <snehal.mahadik@voltmoney.in>
2025-04-11 19:23:11 +0530

    Initial commit

--- src/main/java/pages/BankSetup.java
+++ src/main/java/pages/BankSetup.java
@@ -1,7 +1,6 @@
 package pages;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
@@ -10,7 +9,6 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.time.Duration;
 
-import static java.lang.Thread.sleep;
 
 public class BankSetup{
 
